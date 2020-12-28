d41b19637f1e7889adf499baac153a79c7b14801
Arata Kagan <aratakagan@gmail.com>
2020-12-27 16:09:57 -0800

    Initial Commit

---
+++ LGBTQNews/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LGBTQNews
+//
+//  Created by Arata Kagan on 12/27/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
